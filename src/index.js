ba3367fa1f7c99db5c6403835b606fb53e5090f0
Oleksandr Svyatobog <osvyatobog@gmail.com>
2022-09-05 20:04:56 +0300

    HW11

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+// axios.post('/user', {
+//     firstName: 'Fred',
+//     lastName: 'Flintstone'
+//   })
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+
+// <div class="photo-card">
+//     <img src="" alt="" loading="lazy" />
+//     <div class="info">
+//       <p class="info-item">
+//         <b>Likes</b>
+//       </p>
+//       <p class="info-item">
+//         <b>Views</b>
+//       </p>
+//       <p class="info-item">
+//         <b>Comments</b>
+//       </p>
+//       <p class="info-item">
+//         <b>Downloads</b>
+//       </p>
+//     </div>
+//   </div>
+/* <button type="button" class="load-more">Load more</button> */
