24a3da2f3bda3d7e7286954736c4196a211ae831
Oleksandr Svyatobog <osvyatobog@gmail.com>
2022-09-08 08:22:44 +0300

    Task11- pending

--- src/index.js
+++ src/index.js
@@ -1,33 +1,72 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import axios from "axios";
+import {Notiflix} from "notiflix";
 import './css/styles.css';
 
-// axios.post('/user', {
-//     firstName: 'Fred',
-//     lastName: 'Flintstone'
-//   })
-//   .then(function (response) {
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const refs= {form: document.querySelector('#search-form'),
+loadMoreBtn: document.querySelector('button.load-more'),
+submit: document.querySelector('[type="submit"]'),
+renderDiv: document.querySelector('.gallery'),
+} 
+
+refs.form.addEventListener('submit', onSubmit)
 
 
-// <div class="photo-card">
-//     <img src="" alt="" loading="lazy" />
-//     <div class="info">
-//       <p class="info-item">
-//         <b>Likes</b>
-//       </p>
-//       <p class="info-item">
-//         <b>Views</b>
-//       </p>
-//       <p class="info-item">
-//         <b>Comments</b>
-//       </p>
-//       <p class="info-item">
-//         <b>Downloads</b>
-//       </p>
-//     </div>
-//   </div>
-/* <button type="button" class="load-more">Load more</button> */
+function onSubmit(e) {
+    e.preventDefault();
+    const searchQuery = e.currentTarget.elements.searchQuery.value
+    axios.get(`/?key=29723422-b06f38d2a18d9cab8e97b2e74&q=${searchQuery}&image_type=photo&safesearch=true&orientation=horisontal`)
+  .then(response => {renderPhoto(response.data.hits)});}
+
+// const AUTH_TOKEN = 29723422-b06f38d2a18d9cab8e97b2e74;
+
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+
+// const options = {
+//     headers: {
+//         // AUTH_TOKEN = 29723422-b06f38d2a18d9cab8e97b2e74,
+//         // q: "cat",
+//         image_type:  "photo",
+//         orientation:  "horizontal",
+//         safesearch: true,
+  // }
+// }
+
+//   async function getUser() {
+//     try {
+//       const response = await axios.get('/user?ID=12345');
+//       console.log(response);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+
+function renderPhoto(arr) {
+    const markup = arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+      return `<div class="photo-card">
+      <a class="photo-link" href="${largeImageURL}">
+      <img class="image" src="${webformatURL}" alt="${tags}" loading="lazy"  width="300" height="300"/>
+      </a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes: ${likes}</b>
+        </p>
+        <p class="info-item">
+        <b>Downloads: ${downloads}</b>
+      </p>
+        <p class="info-item">
+          <b>Views: ${views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments: ${comments}</b>
+        </p>
+       
+      </div>
+    </div>`
+    }).join('');
+
+    refs.renderDiv.insertAdjacentHTML('beforeend', markup);
+   
+};
