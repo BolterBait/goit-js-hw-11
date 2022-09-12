f039025dd2e269cbdd6eb48f78bc1c07b0c33d35
Oleksandr Svyatobog <osvyatobog@gmail.com>
2022-09-12 12:24:44 +0300

    Task11 with Simplelightbox

--- src/index.js
+++ src/index.js
@@ -27,27 +27,27 @@ const searchBtn = new SearchBtn({
 refs.form.addEventListener('submit', onSubmit);
 loadMoreBtn.refs.button.addEventListener('click', onLoadMoreBtnClick);
 let searchQuery = '';
-let page = 0;
-let totalHits =0;
-let image =0;
+let page = 1;
+let image = 0;
 let per_page = 40;
+let lightbox = {};
 
-function onSubmit(e) {
+async function onSubmit(e) {
     e.preventDefault();
+    console.log(page);
       searchQuery = e.currentTarget.elements.searchQuery.value.trim();
-      console.log(searchQuery);
-      page = 1;
-      image = 40;
-
-      if (searchQuery === ""){
+        if (searchQuery === ""){
         return Notify.info("Please enter some text!")
       }
-      
-      loadMoreBtn.show()
+       loadMoreBtn.show()
       loadMoreBtn.disable()
-        axios.get(`/?key=${API_KEY}&q=${searchQuery}&image_type=photo&safesearch=true&orientation=horisontal&page=${page}&per_page=${per_page}`)
-  .then(response => {clearPhotoMarkup();
+      try {const response = await axios.get(`/?key=${API_KEY}&q=${searchQuery}&image_type=photo&safesearch=true&orientation=horisontal&page=${page}&per_page=${per_page}`)
+      clearPhotoMarkup();
     renderPhoto(response.data.hits);
+    lightbox = new SimpleLightbox('.gallery a', {
+      captionDelay: 250,
+      captionsData: 'alt',
+    });
     loadMoreBtn.enable();
     if (response.data.hits.length === 0) {
       Notify.failure("Sorry, there are no images matching your search query. Please try again.")
@@ -56,28 +56,32 @@ function onSubmit(e) {
     totalHits = response.data.totalHits;    
   if (totalHits >= 1) {
     Notify.info(`Hooray! We found ${totalHits} images.`)
-  }
-   });
-}
+  }image = totalHits - per_page;
+       } catch (error) {
+      console.error(error);
+      }  
+        };
 
-function onLoadMoreBtnClick(e) {
+async function onLoadMoreBtnClick(e) {
   loadMoreBtn.disable();
-  
-  per_page = totalHits-(per_page * page);
   page += 1;
-  image += per_page;
-  // searchQuery = e.currentTarget.elements.searchQuery.value.trim();
-   axios.get(`/?key=${API_KEY}&q=${searchQuery}&image_type=photo&safesearch=true&orientation=horisontal&page=${page}&per_page=${per_page}`)
-  .then(response => {renderPhoto(response.data.hits);
-    console.log(searchQuery);
-    loadMoreBtn.enable();
-    if (totalHits <= image) {
-      Notify.info("We're sorry, but you've reached the end of search results.")
-      loadMoreBtn.hide();
-    }
-    });
-   
-}
+  if (per_page >= image) {
+    per_page = image;
+  }image -= per_page;
+    
+  try { const response = await axios.get(`/?key=${API_KEY}&q=${searchQuery}&image_type=photo&safesearch=true&orientation=horisontal&page=${page}&per_page=${per_page}`)
+  renderPhoto(response.data.hits);
+  lightbox.refresh()
+  console.log(searchQuery);
+  loadMoreBtn.enable();
+  if (per_page < 40) {
+    Notify.info("We're sorry, but you've reached the end of search results.")
+    loadMoreBtn.hide();
+  }
+  } catch (error) {
+  console.error(error);  
+  } 
+  }
 
 function renderPhoto(arr) {
     const markup = arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
@@ -111,21 +115,6 @@ function clearPhotoMarkup() {
   refs.renderDiv.innerHTML = '';
 }
 
-// async fetchGallery() {
-//   console.log("До запроса:", this);
-// try {
-//   const response = await axios.get(
-//     ?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${this.perPage}
-//   );
-//   this.incrementPage()
-//   console.log(response);
-//   return response.data.hits;
-// } catch (error) {
-//   console.log(error);
-// }
-// }
-  // Notify.info(`Hooray! We found ${totalHits} images.`)
-// const AUTH_TOKEN = 29723422-b06f38d2a18d9cab8e97b2e74;
 
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 
@@ -138,12 +127,3 @@ function clearPhotoMarkup() {
 //         safesearch: true,
   // }
 // }
-
-//   async function getUser() {
-//     try {
-//       const response = await axios.get('/user?ID=12345');
-//       console.log(response);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
