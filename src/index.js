b02a94aeda9b9cc97ef7b22a63ec8ccab07727ce
Oleksandr Svyatobog <osvyatobog@gmail.com>
2022-09-13 17:04:08 +0300

    Task 11 final after mentor check

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { Notify } from "notiflix";
 import './css/styles.css';
 import LoadMoreBtn from './load-more-btn';
-import SearchBtn from "./search-btn";
+import simpleLightbox from "simplelightbox";
 
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
@@ -19,48 +19,51 @@ const loadMoreBtn = new LoadMoreBtn({
   hidden: true,
 });
 
-const searchBtn = new SearchBtn({
-  selector: '[data-action="search"]', 
-  hidden: false,})
-
-  let lightbox = new SimpleLightbox('.gallery a', {
-    captionDelay: 250,
-    captionsData: 'alt',
-  });
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});  
 
 refs.form.addEventListener('submit', onSubmit);
 loadMoreBtn.refs.button.addEventListener('click', onLoadMoreBtnClick);
+
+
 let searchQuery = '';
-let page = 1;
+let page;
 let image = 0;
-let per_page = 40;
+let per_page;
 
 
 async function onSubmit(e) {
     e.preventDefault();
-    console.log(page);
-      searchQuery = e.currentTarget.elements.searchQuery.value.trim();
+        searchQuery = e.currentTarget.elements.searchQuery.value.trim();
+        page = 0;
+        per_page = 40;
         if (searchQuery === ""){
         return Notify.info("Please enter some text!")
       }
-       loadMoreBtn.show()
-      loadMoreBtn.disable()
+       
+           page += 1;
       try {const response = await axios.get(`/?key=${API_KEY}&q=${searchQuery}&image_type=photo&safesearch=true&orientation=horisontal&page=${page}&per_page=${per_page}`)
+      
       clearPhotoMarkup();
     renderPhoto(response.data.hits);
-    lightbox = new SimpleLightbox('.gallery a', {
-      captionDelay: 250,
-      captionsData: 'alt',
-    });
-    loadMoreBtn.enable();
+    lightbox.refresh()
+    
     if (response.data.hits.length === 0) {
+      loadMoreBtn.disable()
       Notify.failure("Sorry, there are no images matching your search query. Please try again.")
       return;
   }
+
+  loadMoreBtn.show()
+     loadMoreBtn.enable();
     const totalHits = response.data.totalHits;    
+
   if (totalHits >= 1) {
     Notify.info(`Hooray! We found ${totalHits} images.`)
-  }image = totalHits - per_page;
+  }
+  image = totalHits - per_page;
        } catch (error) {
       console.error(error);
       }  
@@ -76,7 +79,15 @@ async function onLoadMoreBtnClick(e) {
   try { const response = await axios.get(`/?key=${API_KEY}&q=${searchQuery}&image_type=photo&safesearch=true&orientation=horisontal&page=${page}&per_page=${per_page}`)
   renderPhoto(response.data.hits);
   lightbox.refresh()
-  console.log(searchQuery);
+  
+  const { height: cardHeight } = document
+.querySelector(".gallery")
+.firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: "smooth",
+  });
+  
   loadMoreBtn.enable();
   if (per_page < 40) {
     Notify.info("We're sorry, but you've reached the end of search results.")
@@ -112,15 +123,7 @@ function renderPhoto(arr) {
     }).join('');
 
     refs.renderDiv.insertAdjacentHTML('beforeend', markup);
-    const { height: cardHeight } = document
-  .querySelector(".gallery")
-  .firstElementChild.getBoundingClientRect();
-
-window.scrollBy({
-  top: cardHeight * 2,
-  behavior: "smooth",
-});
-    
+       
 };
 
 function clearPhotoMarkup() {
@@ -128,14 +131,3 @@ function clearPhotoMarkup() {
 }
 
 
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-
-// const options = {
-//     headers: {
-//         // AUTH_TOKEN = 29723422-b06f38d2a18d9cab8e97b2e74,
-//         // q: "cat",
-//         image_type:  "photo",
-//         orientation:  "horizontal",
-//         safesearch: true,
-  // }
-// }
